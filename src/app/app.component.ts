e3847a6b09b114a03f005380eb71e9cefcfa9705
sayed-soeb <sayed.soeb1@gmail.com>
2024-01-17 21:37:27 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,37 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-todoapp';
+  list: any[] = [];
+
+  ngOnInit(): void {
+    this.retrieveTodos();
+  }
+
+  addTodo(item: string) {
+    this.list.push({ id: this.list.length, todo: item, status: false });
+    this.storeTodos();
+  }
+
+  update(ids: number) {
+    this.list[ids].status = !this.list[ids].status;
+    this.storeTodos();
+  }
+
+  deleteTodo(id: number) {
+    this.list = this.list.filter(item => item.id !== id);
+    this.storeTodos();
+  }
+
+  storeTodos() {
+    localStorage.setItem('todos', JSON.stringify(this.list));
+  }
+
+  retrieveTodos() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      this.list = JSON.parse(storedTodos);
+    }
+  }
 }
